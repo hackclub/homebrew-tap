57dc8fc35cc548948ccd5a46a68d0be75401dd3c
Caleb Denio <cjdenio44@gmail.com>
2021-10-12 23:39:49 -0400

    initial testing

---
+++ Formula/haas.rb
@@ -0,0 +1,13 @@
+class Haas < Formula
+  desc "CLI for Hack as a Service"
+  homepage "https://hackclub.app"
+  version "0.0.0"
+  bottle :unneeded
+
+  url "https://doggo.ninja/oogGOO.gz"
+  sha256 "13fad4cbeabb51ab37912027b88f5a8b9538a9970c2121874941497821c7d695"
+
+  def install
+    bin.install "haas"
+  end
+end
