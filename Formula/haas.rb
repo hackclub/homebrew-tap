a92187ab5ab128f0753a4f68f03c4019f20984bc
Caleb Denio <cjdenio44@gmail.com>
2021-10-14 12:02:46 -0400

    update haas

--- Formula/haas.rb
+++ Formula/haas.rb
@@ -2,12 +2,11 @@ class Haas < Formula
   desc "CLI for Hack as a Service"
   homepage "https://hackclub.app"
   version "0.0.0"
-  bottle :unneeded
 
-  url "https://doggo.ninja/oogGOO.gz"
-  sha256 "13fad4cbeabb51ab37912027b88f5a8b9538a9970c2121874941497821c7d695"
+  url "https://doggo.ninja/XpkiGj.gz"
+  sha256 "97e76ba67d2b5f0c9d02482fa3f0957f6c04affeee1f5436af62bd876db609d7"
 
   def install
-    bin.install "haas"
+    bin.install "haas_cli" => "haas"
   end
 end
